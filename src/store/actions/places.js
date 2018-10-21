b82f9bb5cfe54fb72323d3780f5962683236ce23
Erkan Ertürk <erkanerturk096@gmail.com>
2018-10-21 18:12:00 +0300

    init

---
+++ src/store/actions/places.js
@@ -0,0 +1,19 @@
+import { ADD_PLACE, DELETE_PLACE, SELECT_PLACE, DESELECT_PLACE } from './actionTypes';
+
+export const addPlace = placeName => ({
+  type: ADD_PLACE,
+  placeName: placeName,
+});
+
+export const deletePlace = () => ({
+  type: DELETE_PLACE,
+});
+
+export const selectPlace = key => ({
+  type: SELECT_PLACE,
+  placeKey: key,
+});
+
+export const deselectPlace = () => ({
+  type: DESELECT_PLACE,
+});
---
+++ src/store/reducers/places.js
@@ -0,0 +1,44 @@
+import { ADD_PLACE, DELETE_PLACE, SELECT_PLACE, DESELECT_PLACE } from '../actions/actionTypes';
+
+const initialState = {
+  places: [],
+  selectedPlace: null,
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_PLACE:
+      return {
+        ...state,
+        places: state.places.concat({
+          key: Math.random().toString(),
+          name: action.placeName,
+          image: {
+            uri: 'https://c1.staticflickr.com/5/4096/4744241983_34023bf303_b.jpg',
+          },
+        }),
+      };
+
+    case DELETE_PLACE:
+      return {
+        ...state,
+        places: state.places.filter(place => place.key !== state.selectedPlace.key),
+        selectedPlace: null,
+      };
+
+    case SELECT_PLACE:
+      return {
+        ...state,
+        selectedPlace: state.places.find(place => place.key === action.placeKey),
+      };
+
+    case DESELECT_PLACE:
+      return {
+        ...state,
+        selectedPlace: null,
+      };
+
+    default:
+      return state;
+  }
+};
