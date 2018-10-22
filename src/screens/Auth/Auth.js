950750b8548f99dee70843e32471ec3709ecff8f
Erkan Ertürk <erkanerturk096@gmail.com>
2018-10-22 15:45:26 +0300

    Global style - Custom component

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
-
+import { View, Text, Button, StyleSheet } from 'react-native';
 
 import startMainTabs from '../MainTabs/startMainTabs';
+import DefaultInput from '../../components/UI/DefaultInput/DefaultInput';
+import HeadingText from '../../components/UI/HeadingText/HeadingText';
+import MainText from '../../components/UI/MainText/MainText';
 
 class AuthScreen extends Component {
   loginHandler = () => {
@@ -11,12 +13,37 @@ class AuthScreen extends Component {
 
   render() {
     return (
-      <View>
-        <Text>Auth Screen</Text>
-        <Button title="Login" onPress={this.loginHandler} />
+      <View style={styles.container}>
+        <MainText>
+          <HeadingText>Please Log In</HeadingText>
+        </MainText>
+        <Button title="Switch to Login" onPress={this.loginHandler} />
+        <View style={styles.inputContainer}>
+          <DefaultInput placeholder="Your E-Mail Address" style={styles.input} />
+          <DefaultInput placeholder="Password" style={styles.input} />
+          <DefaultInput placeholder="Confirm Password" style={styles.input} />
+        </View>
+        <Button title="Submit" onPress={this.loginHandler} />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  inputContainer: {
+    width: '80%',
+    alignItems: 'center',
+  },
+  textHeading: {},
+  input: {
+    backgroundColor: '#eee',
+    borderColor: '#bbb',
+  },
+});
 
 export default AuthScreen;
