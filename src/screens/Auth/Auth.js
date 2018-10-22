e38f3e032fd14a45f61ed8c22b15d14206996778
Erkan Ertürk <erkanerturk096@gmail.com>
2018-10-22 16:48:02 +0300

    re-usable custom button

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { View, Button, StyleSheet, ImageBackground } from 'react-native';
+import { View, StyleSheet, ImageBackground } from 'react-native';
 
 import startMainTabs from '../MainTabs/startMainTabs';
 import DefaultInput from '../../components/UI/DefaultInput/DefaultInput';
 import HeadingText from '../../components/UI/HeadingText/HeadingText';
 import MainText from '../../components/UI/MainText/MainText';
+import ButtonWithBackground from '../../components/UI/ButtonWithBackground/ButtonWithBackground';
 
 import backgroundImage from '../../assets/background.jpg';
 
@@ -20,13 +21,17 @@ class AuthScreen extends Component {
           <MainText>
             <HeadingText>Please Log In</HeadingText>
           </MainText>
-          <Button title="Switch to Login" onPress={this.loginHandler} />
+          <ButtonWithBackground onPress={() => alert('Hey')} color="#29aaf4">
+            Switch to Login
+          </ButtonWithBackground>
           <View style={styles.inputContainer}>
             <DefaultInput placeholder="Your E-Mail Address" style={styles.input} />
             <DefaultInput placeholder="Password" style={styles.input} />
             <DefaultInput placeholder="Confirm Password" style={styles.input} />
           </View>
-          <Button title="Submit" onPress={this.loginHandler} />
+          <ButtonWithBackground onPress={this.loginHandler} color="#29aaf4">
+            Submit
+          </ButtonWithBackground>
         </View>
       </ImageBackground>
     );
@@ -47,7 +52,6 @@ const styles = StyleSheet.create({
     width: '80%',
     alignItems: 'center',
   },
-  textHeading: {},
   input: {
     backgroundColor: '#eee',
     borderColor: '#bbb',
