4db8235a04b9462aeadaa5576c73c0f90e961316
Erkan Ertürk <erkanerturk096@gmail.com>
2018-10-22 21:17:15 +0300

    Finished SharePlace

--- src/screens/SharePlace/SharePlace.js
+++ src/screens/SharePlace/SharePlace.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Text, Button, StyleSheet, ScrollView, Image } from 'react-native';
 import { connect } from 'react-redux';
 
 import PlaceInput from '../../components/PlaceInput/PlaceInput';
+import PickImage from '../../components/PickImage/PickImage';
+import PickLocation from '../../components/PickLocation/PickLocation';
+import MainText from '../../components/UI/MainText/MainText';
+import HeadingText from '../../components/UI/HeadingText/HeadingText';
+
 import { addPlace } from '../../store/actions/index';
 
 class SharePlaceScreen extends Component {
+  state = {
+    placeName: '',
+  };
+
   constructor(props) {
     super(props);
     this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent);
@@ -22,18 +31,61 @@ class SharePlaceScreen extends Component {
     }
   };
 
-  placeAddedHandler = placeName => {
-    this.props.onAddPlace(placeName);
+  placeNameChangedHandler = placeName => {
+    this.setState({
+      placeName,
+    });
+  };
+
+  placeAddedHandler = () => {
+    const { placeName } = this.state;
+    if (placeName.trim() !== '') {
+      this.props.onAddPlace(placeName);
+    }
   };
 
   render() {
     return (
-      <View>
-        <PlaceInput onPlaceAdded={this.placeAddedHandler} />
-      </View>
+      <ScrollView>
+        <View style={styles.container}>
+          <MainText>
+            <HeadingText>Share a place with us</HeadingText>
+          </MainText>
+          <PickImage />
+          <PickLocation />
+          <PlaceInput
+            placeName={this.state.placeName}
+            onChangeText={this.placeNameChangedHandler}
+          />
+          <View style={styles.button}>
+            <Button title="Share the Place" onPress={this.placeAddedHandler} />
+          </View>
+        </View>
+      </ScrollView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  placeholder: {
+    borderWidth: 1,
+    borderColor: 'black',
+    backgroundColor: '#eee',
+    width: '80%',
+    height: 150,
+  },
+  button: {
+    margin: 8,
+  },
+  previewImage: {
+    width: '100%',
+    height: '100%',
+  },
+});
 
 const mapDispatchToProps = dispatch => {
   return {
